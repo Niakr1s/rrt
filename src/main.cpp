fcdf134a4b6e4f42062033a44c253a83abff60b1
Niakr1s <pavel2188@gmail.com>
2019-11-16 23:00:02 +0300

    main for some tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <algorithm>
 #include <iostream>
 #include <stdexcept>
+#include "db.h"
+#include "sqldb.h"
+#include "xml.h"
 
 int main(int argc, char* argv[]) {
+  rrt::DB::set<rrt::SqlDB>();
+  auto xml = rrt::XML("KPT CadastralBlock 77 03 0009007.xml");
+  for (auto& spatial : xml.xmlSpatials()) {
+    rrt::DB::get()->pushToDB(*spatial);
+  }
   return 0;
 }
