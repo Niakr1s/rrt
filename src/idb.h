a6db6adf4671e44a08ef78e1b27990632406770a
Niakr1s <pavel2188@gmail.com>
2019-11-16 16:40:59 +0300

    completed IDB interface instantiation

--- src/idb.h
+++ src/idb.h
@@ -1,11 +1,20 @@
 #ifndef IDB_H
 #define IDB_H
 
+#include <memory>
+#include "cadastralnumber.h"
+#include "xmlspatial.h"
+
 namespace rrt {
 
 class IDB {
  public:
   IDB();
+  virtual ~IDB() = default;
+
+  virtual void pushToDB(const XMLSpatial& xmlSpatial) = 0;
+  virtual std::shared_ptr<XMLSpatial> getFromDB(
+      const CadastralNumber& cadastralNumber) = 0;
 };
 
 }  // namespace rrt
