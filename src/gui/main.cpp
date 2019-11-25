e321e21a3aa6038dda04d7854c3b279879136f45
Niakr1s <pavel2188@gmail.com>
2019-11-25 20:21:15 +0300

    Revert "timestamps in log"
    This reverts commit 4a897ca58e6cb28f2df5c9cd924f3c5b48a0f8ca.

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -13,9 +13,8 @@ void registerMetaTypes() {
 }
 
 void logToFile() {
-  boost::log::add_file_log(
-      boost::log::keywords::file_name = "rrt.log",
-      boost::log::keywords::format = "[%TimeStamp%]: %Message%");
+  boost::log::add_file_log(boost::log::keywords::file_name = "rrt.log",
+                           boost::log::keywords::auto_flush = true);
   boost::log::core::get()->set_filter(boost::log::trivial::severity >=
                                       boost::log::trivial::debug);
 }
